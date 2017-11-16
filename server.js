8069431d675ee7a98a7def616f1eb5f9cbdc1672
Nkosinathi <nathi4sure@gmail.com>
2017-11-16 19:54:01 +0200

    added a few more modules

--- server.js
+++ server.js
@@ -3,9 +3,33 @@ var app = express();
 var router = require('./router/router.js');
 var bodyParser = require("body-parser");
 var packagejson = require("./package.json");
+var logger = require("morgan");
+var favicon = require("serve-favicon");
+var path = require("path");
+var session = require("express-session");
+var passport = require("passport");
+var localStrategy = require("passport-local").Strategy;
+var multer = require("multer");
+var flash = require("connect-flash");
+var mongoose = require("mongoose");
 
 app.use(bodyParser.json())
+app.use(express.static('public'));
+app.use(multer({dest:'./uploads'}));
+app.use(session(
+    {
+        secret: 'secrete',
+        saveUninitialized:true,
+        resave: true
+    }
+))
 
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
+
+//app.use('/', express.static(__dirname + '/www')); // redirect root
 app.post('/login', router);
 app.get('/log', router);
 app.listen(packagejson.port, ()=>{
